7e6e6693978b493e163c62d8625e3cacbb385238
eony <eony1010@naver.com>
2021-05-23 15:24:37 +0900

    first commit

---
+++ src/main/java/com/example/dabok/DabokApplication.java
@@ -0,0 +1,13 @@
+package com.example.dabok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabokApplication.class, args);
+	}
+
+}

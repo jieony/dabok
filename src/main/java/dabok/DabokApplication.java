33a82405888a5920390caf1b8b99f57836ae76ce
eony <eony1010@naver.com>
2021-05-23 17:38:15 +0900

    Hello 👋 생성, 프로젝트 구조 추가

--- src/main/java/dabok/DabokApplication.java
+++ src/main/java/dabok/DabokApplication.java
@@ -1,4 +1,4 @@
-package com.example.dabok;
+package dabok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dabok/DabokApplicationTests.java
+++ src/test/java/dabok/DabokApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.dabok;
+package dabok;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

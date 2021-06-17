b983e6e490ecf9e04672863e59e70ffb54c21aac
dev-ella13 <hyeonyeongn95@gmail.com>
2021-06-18 00:56:20 +0900

    [Hyeon_210618] Address수정

--- src/main/java/dabok/domain/Address.java
+++ src/main/java/dabok/domain/Address.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String street;
@@ -15,4 +15,13 @@ public class Address {
     private String detail;
 
     private String zipcode;
+
+    protected Address(){
+    }
+
+    public Address(String street, String detail, String zipcode){
+        this.street = street;
+        this.detail = detail;
+        this.zipcode = zipcode;
+    }
 }
